6893a5bc4cbe0ff0d20e9239f37eb86c9977e441
andrecp <andreprado88@gmail.com>
2015-12-12 22:06:07 +0000

    shorten: adding service to lookup urls by shorten

--- app/url_shortener/services.py
+++ app/url_shortener/services.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+from app import db
+from app import login_manager
+from .models import URL
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
+
+
+def lookup_url_by_shorten(url):
+    """Lookup the URL given a shorten URL"""
+
+    try:
+        url = URL.query.filter_by(shorten_url=url).one()
+    except NoResultFound:
+        return None
+    return url
