ca00f5d049b5629ec806415f8cdab2d2f8d5964f
andrecp <andreprado88@gmail.com>
2015-12-12 10:18:16 +0000

    home: use macro to render fields, add login/sign in/logout templates.

--- app/url_shortener/forms.py
+++ app/url_shortener/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import Form
 from wtforms import StringField
+from wtforms.validators import Length
 from wtforms.validators import URL
 from wtforms.validators import DataRequired
 
@@ -9,6 +10,7 @@ class CreateURLForm(Form):
             'Input URL',
             validators=[
                     URL(require_tld=True, message=u'Invalid URL.'),
-                    DataRequired()])
+                    DataRequired(),
+                    Length(max=23)])
 
     shorten_url = StringField('Shorten URL', validators=[URL()])
