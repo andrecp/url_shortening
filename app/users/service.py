a7c29ccd104f49973829830cbea194c33d7d8c9e
andrecp <andreprado88@gmail.com>
2015-12-12 09:38:17 +0000

    sevices: renaming file, adding service to create an user and load user properly from db.

--- app/users/service.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import flask.ext.login as flask_login
-
-from .models import User
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
---
+++ app/users/services.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from app import db
+from app import login_manager
+from .models import User
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    try:
+        user = User.query.filter_by(id=int(user_id)).one()
+    except NoResultFound:
+        return None
+    return user
+
+
+def create_user(username, password):
+    """Service to create a new user"""
+
+    user = User(username=username, password=password)
+    # Insert the user in the database
+    db.session.add(user)
+    try:
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        return None
+
+    return user
